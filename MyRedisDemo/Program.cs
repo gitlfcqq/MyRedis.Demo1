4196045410f81c6eead1a8bc88ab804e2a2030e9
longfc <longfuchu@163.com>
2021-05-24 22:19:06 +0800

    5

--- MyRedisDemo/Program.cs
+++ MyRedisDemo/Program.cs
@@ -9,13 +9,22 @@ namespace MyRedisDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
             //调用Redis方法
-
             RedisBaseHelp redis = new RedisBaseHelp();
+            
+
+
+        }
+        #region 操作String
+        private void TestString(RedisBaseHelp redis)
+        {
             redis.StringSet("keyname", "1234567", new TimeSpan(0, 10, 0));
+<<<<<<< HEAD
+=======
 
             #region 操作String
+>>>>>>> 0e70eea49cb258f7aa82decb520a709aeac253e0
             string key1 = redis.StringGet("keyname");
             Console.WriteLine($"key1:{key1}");
             #endregion
@@ -34,5 +43,20 @@ namespace MyRedisDemo
 
 
         }
+        #endregion
+
+        #region 操作对象
+        private void TestObj(RedisBaseHelp redis)
+        {
+            var yangUser = new User { Id = 1, Name = "Eric Yang" };
+            var zhangUser = new User { Id = 2, Name = "Fish Zhang" };
+            bool result1 = redis.StringSet<User>("yangUser", yangUser);
+            bool result2 = redis.StringSet<User>("zhangUser", zhangUser);
+            var yangUser2 = redis.StringGet<User>("yangUser");
+            var zhangUser2 = redis.StringGet<User>("zhangUser");
+            Console.WriteLine($"yangUser:{result1}");
+            Console.WriteLine($"zhangUser:{result2}");
+        }
+        #endregion
     }
 }
---
+++ RedisSub/Program.cs
@@ -0,0 +1,49 @@
+﻿using ServiceStack.Redis;
+using System;
+
+/// <summary>
+/// 创建一个RedisSub项目用来模拟订阅客户端
+/// </summary>
+namespace RedisSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				using (RedisClient consumer = new RedisClient("127.0.0.1", 6379))
+				{
+
+					Console.WriteLine($"这是订阅客户端");
+					var subscription = consumer.CreateSubscription();
+					//接受到消息时
+					subscription.OnMessage = (channel, msg) =>
+					{
+						if (msg != "CTRL:PULSE")
+						{
+							//此处可以写入日志记录
+
+						}
+
+					};
+					//订阅频道时
+					subscription.OnSubscribe = (channel) =>
+					{
+						Console.WriteLine("订阅客户端：开始订阅" + channel);
+					};
+					//取消订阅频道时
+					subscription.OnUnSubscribe = (a) => { Console.WriteLine("订阅客户端：取消订阅"); };
+					//订阅频道
+					string topicname = "Tom is a good cat";
+					subscription.SubscribeToChannels(topicname);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+		}
+    }
+}
